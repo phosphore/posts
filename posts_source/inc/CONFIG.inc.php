938c6495dab1ae13645841c83ed15b5be3d78210
gus <>
2010-12-11 00:08:12 -0500

    changed vars from define to const

--- posts_source/inc/CONFIG.inc.php
+++ posts_source/inc/CONFIG.inc.php
@@ -1,16 +1,15 @@
 <?php
+const PDO_DB_TYPE = "";
+const PDO_DB_HOST = "";
+const PDO_DB_USER = "";
+const PDO_DB_NAME = "";
+const PDO_DB_PASSWORD= "";
 
-define("PDO_DB_TYPE","");
-define("PDO_DB_HOST","");
-define("PDO_DB_USER","");
-define("PDO_DB_NAME","");
-define("PDO_DB_PASSWORD","");
+const BASE_URL = "http://localhost/posts/posts_source/core/";
+const DEBUG = true;
+const PAGES = 7; // should be > 2
 
-define("BASE_URL","http://localhost/posts/posts_source/core/");
-define("DEBUG",true);
-define("PAGES",7); // should be > 2
-
-define("REPLIES_PER_PAGE",2);
-define("TOPICS_PER_PAGE",4);
+const REPLIES_PER_PAGE = 2;
+const TOPICS_PER_PAGE = 4;
 
 ?>
