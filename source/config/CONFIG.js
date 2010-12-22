003aafa09bbcc250ae6b2c90959766e7addbd8b4
gus <>
2010-12-21 23:51:08 -0500

    changed url so it's seo friendly

--- source/config/CONFIG.js
+++ source/config/CONFIG.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   'TOPIC_UPDATE_INTERVAL': 8000, 
   'REPLY_UPDATE_INTERVAL': 16000,
-  'REPLY_PAGE' : 'http://localhost/posts/source/core/reply.php?id=',
+  'REPLY_PAGE' : 'http://localhost/posts/source/core/reply/',
   'BASE_URL' : 'http://localhost/posts/source/core/'
 };
